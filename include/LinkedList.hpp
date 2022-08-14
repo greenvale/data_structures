ea5abf50a43368bbcc5c33060a4006e312252fb2
William Denny <wdenny1999@gmail.com>
2022-08-14 21:03:26 +0100

    Added double linked node

--- include/LinkedList.hpp
+++ include/LinkedList.hpp
@@ -2,6 +2,8 @@
 
 namespace DataStructLib
 {
+    // SINGLE LINKED NODE
+    
     template <class T> 
     class SingleNode
     {
@@ -68,4 +70,100 @@ namespace DataStructLib
     {
         m_nextPtr = nextPtr;
     }
+    
+    // =============================================================================
+    // =============================================================================
+    // =============================================================================
+    // DOUBLE LINKED NODE
+    
+    template <class T> 
+    class DoubleNode
+    {
+    private:
+        T m_value;
+        DoubleNode* m_prevPtr;
+        DoubleNode* m_nextPtr;
+    public:
+        DoubleNode();
+        DoubleNode(const T& value);
+        DoubleNode(DoubleNode* prevPtr, DoubleNode* nextPtr);
+        DoubleNode(const T& value, DoubleNode* prevPtr, DoubleNode* nextPtr);      
+        
+        T getValue() const;
+        void setValue(const T& value);
+        
+        DoubleNode* getPrevPtr() const;
+        DoubleNode* getNextPtr() const;
+        void setPrevPtr(DoubleNode* prevPtr);
+        void setNextPtr(DoubleNode* nextPtr);
+    };
+    
+    template <class T>
+    DoubleNode<T>::DoubleNode()
+    {
+    }
+    
+    template <class T>
+    DoubleNode<T>::DoubleNode(const T& value)
+    {
+        m_value = value;
+    }
+    
+    template <class T>
+    DoubleNode<T>::DoubleNode(DoubleNode* prevPtr, DoubleNode* nextPtr)
+    {
+        m_prevPtr = prevPtr;
+        m_nextPtr = nextPtr;
+    }
+    
+    template <class T>
+    DoubleNode<T>::DoubleNode(const T& value, DoubleNode* prevPtr, DoubleNode* nextPtr)
+    {
+        m_value = value;
+        m_prevPtr = prevPtr;
+        m_nextPtr = nextPtr;
+    }
+    
+    template <class T>
+    T DoubleNode<T>::getValue() const
+    {
+        return m_value;
+    }
+    
+    template <class T>
+    void DoubleNode<T>::setValue(const T& value)
+    {
+        m_value = value;
+    }
+    
+    template <class T>
+    DoubleNode<T>* DoubleNode<T>::getPrevPtr() const
+    {
+        return m_prevPtr;
+    }
+    
+    template <class T>
+    DoubleNode<T>* DoubleNode<T>::getNextPtr() const
+    {
+        return m_nextPtr;
+    }
+    
+    template <class T>
+    void DoubleNode<T>::setPrevPtr(DoubleNode* prevPtr) 
+    {
+        m_prevPtr = prevPtr;
+    }
+    
+    template <class T>
+    void DoubleNode<T>::setNextPtr(DoubleNode* nextPtr) 
+    {
+        m_nextPtr = nextPtr;
+    }
+    
+    // =============================================================================
+    // =============================================================================
+    // =============================================================================
+    // LINKED LIST
+    
+    
 }
--- testing/main.cpp
+++ testing/main.cpp
@@ -7,13 +7,17 @@ int main()
 {
     float num1 = 1.0;
     float num2 = 2.0;
+    float num3 = 3.0;
     
-    DataStructLib::SingleNode<float> node1(num1);
-    DataStructLib::SingleNode<float> node2(num2);
+    DataStructLib::DoubleNode<float> node1(num1);
+    DataStructLib::DoubleNode<float> node2(num2);
+    DataStructLib::DoubleNode<float> node3(num3);
     
     node1.setNextPtr(&node2);
     
-    std::cout << node1.getValue() << std::endl;
-    std::cout << node1.getNextPtr() << std::endl;
-    std::cout << (*node1.getNextPtr()).getValue() << std::endl;
+    node2.setPrevPtr(&node1);
+    node2.setNextPtr(&node3);
+    
+    node3.setPrevPtr(&node2);
+    
 }
