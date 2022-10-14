ab58610738d0ba00395a975ae9603bcb3f907b6d
William Denny <wdenny1999@gmail.com>
2022-10-14 14:58:29 +0100

    Added seperate chaining with different data types

--- include/HashTable.hpp
+++ include/HashTable.hpp
@@ -1,8 +1,14 @@
 /* Hash table - W Denny
+    - uses functors for hashing function and probing function
+    - supports seperate-chaining and open-addressing collision resolution methods
+    - seperate-chaining is flexible for different data structure objects by means of an interface class object
+    - this interface class object must be defined for a given datatype - currently defined for own-made DynamicArray and SinglyLinkedList data structure classes
+    - upon creating seperate-chaining object you pass the interface derived class for the required data type
 */
 #pragma once
 
-#include <DynamicArray.hpp>
+#include "DynamicArray.hpp"
+#include "SinglyLinkedList.hpp"
 #include <assert.h>
 
 namespace datastructlib
@@ -24,6 +30,7 @@ public:
     }
     virtual unsigned int operator()(const T& key, const unsigned int& arrLength)
     {
+        std::cout << "Error in hash functor derivation" << std::endl;
         return 0;
     }
 };
@@ -61,7 +68,7 @@ private:
     unsigned int m_a;
     unsigned int m_b;
 public:
-    LinearProbeFunctor() = delete;
+    //LinearProbeFunctor() = delete;
     LinearProbeFunctor(const unsigned int& a, const unsigned int& b)
     {
         assert(a % 2 != 0); // a must not be even - this is because array length in hash table will always be even
@@ -93,6 +100,12 @@ class KeyValPair
 public:
     T m_key;
     U m_val;
+    KeyValPair() {}
+    KeyValPair(const T& key, const U& val)
+    {
+        this->m_key = key;
+        this->m_val = val;
+    }
 };
 
 /********************************************************************************************************************/
@@ -119,7 +132,7 @@ protected:
     unsigned int m_numEntries;
     HashFunctor<T>* m_hashFunctionPtr;
 public:
-    double loadFactor()
+    double loadFactor() const
     {
         return ((double) this->m_numEntries) / ((double) this->m_arrLength);
     }
@@ -128,51 +141,128 @@ public:
 /********************************************************************************************************************/
 /* HASH TABLE WITH SEPERATE CHAINING CLASS */
 
+/* every data structure used for seperate chaining will have different insertion/removal calling functions - this is a general interface functor that generalises these 
+this is the base class from which each data structure will have a derived functor class
+*/
 template <class T, class U>
+class HashTable_DataStructInterface
+{
+public:
+    HashTable_DataStructInterface() {}
+    virtual U get(const T& ds, const unsigned int& ind) {
+        std::cout << "Error in data struct interface derivation" << std::endl;
+        return U(0);
+    }
+    virtual void append(T& ds, U kvpPtr) {std::cout << "Error in data struct interface derivation" << std::endl;}
+    virtual void remove(T& ds, const unsigned int& ind) {std::cout << "Error in data struct interface derivation" << std::endl;}
+    virtual unsigned int length(const T& ds) {
+        std::cout << "Error in data struct interface derivation" << std::endl;
+        return 0;
+    }
+};
+
+/* interface for dynamic array class */
+template <class T, class U>
+class HashTable_DynamicArrayInterface : virtual public HashTable_DataStructInterface< DynamicArray<KeyValPair<T, U>*>, KeyValPair<T, U>* >
+{
+public:
+    KeyValPair<T, U>* get(const DynamicArray<KeyValPair<T, U>*>& ds, const unsigned int& ind)
+    {
+        return ds.get(ind);
+    }
+    void append(DynamicArray<KeyValPair<T, U>*>& ds, KeyValPair<T, U>* kvpPtr)
+    {
+        ds.append(kvpPtr);
+    }
+
+    void remove(DynamicArray<KeyValPair<T, U>*>& ds, const unsigned int& ind)
+    {
+        ds.remove(ind);
+    }
+    unsigned int length(const DynamicArray<KeyValPair<T, U>*>& ds)
+    {
+        return ds.length();
+    }
+};
+
+/* interface for singly-linked list */
+template <class T, class U>
+class HashTable_SinglyLinkedListInterface : virtual public HashTable_DataStructInterface< SinglyLinkedList<KeyValPair<T, U>*>, KeyValPair< T, U>* >
+{
+public:
+    
+    KeyValPair<T, U>* get(const SinglyLinkedList<KeyValPair<T, U>*>& ds, const unsigned int& ind)
+    {
+        SingleNode<KeyValPair<T, U>*>* nodePtr = ds.getPtr(ind);
+        return nodePtr->getVal();
+    }
+    void append(SinglyLinkedList<KeyValPair<T, U>*>& ds, KeyValPair<T, U>* kvpPtr)
+    {
+        SingleNode<KeyValPair<T, U>*>* nodePtr = new SingleNode<KeyValPair<T, U>*>(kvpPtr);
+        ds.insertTail(nodePtr);
+    }
+    void remove(SinglyLinkedList<KeyValPair<T, U>*>& ds, const unsigned int& ind)
+    {
+        ds.remove(ind);
+    }
+    unsigned int length(const SinglyLinkedList<KeyValPair<T, U>*>& ds)
+    {
+        return ds.length();
+    }
+};
+
+/* Seperate chaining hash table 
+*/
+template <class T, class U, class V>
 class HashTable_SeperateChaining : public HashTable<T, U>
 {
 private:
-    DynamicArray<KeyValPair<T, U>>* m_data;
+    V* m_data; // array of data structures of type V containing ptrs to key-value pairs
+    HashTable_DataStructInterface<V, KeyValPair<T, U>*>* m_dataStructInterfacePtr; // interface for manipulating general data structure (as each type has different calling functions, they require individual interface)
 public:
     HashTable_SeperateChaining() = delete;
-    HashTable_SeperateChaining(const unsigned int& length, HashFunctor<T>* hashFunctionPtr);
+    HashTable_SeperateChaining(const unsigned int& length, HashTable_DataStructInterface<V, KeyValPair<T, U>*>* dataStructInterfacePtr, HashFunctor<T>* hashFunctionPtr);
     ~HashTable_SeperateChaining();
-    HashTable_SearchResult<T, U> find(const T& key);
+    HashTable_SearchResult<T, U> find(const T& key) const;
     void insert(const T& key, const U& val);
     bool remove(const T& key);
+    void display();
 };
 
 /* ctor */
-template <class T, class U>
-HashTable_SeperateChaining<T, U>::HashTable_SeperateChaining(const unsigned int& length, HashFunctor<T>* hashFunctionPtr)
+template <class T, class U, class V>
+HashTable_SeperateChaining<T, U, V>::HashTable_SeperateChaining(const unsigned int& length, HashTable_DataStructInterface<V, KeyValPair<T, U>*>* dataStructInterfacePtr, HashFunctor<T>* hashFunctionPtr)
 {
     this->m_arrLength = length;
-    this->m_data = new DynamicArray<KeyValPair<T, U>>[length];
+    this->m_data = new V[length]; // create array of data structures of type V containing ptrs to key-value pairs
     this->m_hashFunctionPtr = hashFunctionPtr;
+    this->m_dataStructInterfacePtr = dataStructInterfacePtr;
     this->m_numEntries = 0;
 }
 
 /* dtor */
-template <class T, class U>
-HashTable_SeperateChaining<T, U>::~HashTable_SeperateChaining()
+template <class T, class U, class V>
+HashTable_SeperateChaining<T, U, V>::~HashTable_SeperateChaining()
 {
+    // finish this function properly
     delete[] this->m_data;
 }
 
 /* returns the pointer to a key value pair given key */
-template <class T, class U>
-HashTable_SearchResult<T, U> HashTable_SeperateChaining<T, U>::find(const T& key)
+template <class T, class U, class V>
+HashTable_SearchResult<T, U> HashTable_SeperateChaining<T, U, V>::find(const T& key) const
 {  
     unsigned int hashedKey = (*this->m_hashFunctionPtr)(key, this->m_arrLength);
-    for (unsigned int i = 0; i < this->m_data[hashedKey].length(); ++i)
+    for (unsigned int i = 0; i < this->m_dataStructInterfacePtr->length(this->m_data[hashedKey]); ++i)
     {
-        if (this->m_data[hashedKey].get(i).m_key == key)
+        if (this->m_dataStructInterfacePtr->get(this->m_data[hashedKey], i)->m_key == key)
         {
+            std::cout << "Found" << std::endl;
             // found
             HashTable_SearchResult<T, U> result;
             result.m_hashedKey = hashedKey;
             result.m_sepChainInd = i;
-            result.m_kvpPtr = this->m_data[hashedKey].getPtr(i); // no copying of key-value, just a ptr to the object in the hash table
+            result.m_kvpPtr = this->m_dataStructInterfacePtr->get(this->m_data[hashedKey], i); // use data structure interface to get ptr from chain
             return result;
         }
     }
@@ -183,31 +273,48 @@ HashTable_SearchResult<T, U> HashTable_SeperateChaining<T, U>::find(const T& key
 }
 
 /* inserts new entry into hash table */
-template <class T, class U>
-void HashTable_SeperateChaining<T, U>::insert(const T& key, const U& val)
+template <class T, class U, class V>
+void HashTable_SeperateChaining<T, U, V>::insert(const T& key, const U& val)
 {
     unsigned int hashedKey = (*this->m_hashFunctionPtr)(key, this->m_arrLength);
     std::cout << "Insertion : Key " << key << " maps onto index " << hashedKey << "\t";
-    std::cout << "Dynamic array @ index " << hashedKey << " has " << this->m_data[hashedKey].length() << " existing entry/entries" << std::endl;
-    KeyValPair<T, U> kvp; // copies the key and value into new key-value pair object
-    kvp.m_key = key;
-    kvp.m_val = val;
-    this->m_data[hashedKey].append(kvp); // copies the key-value pair object into the array
+    std::cout << "Chain @ index " << hashedKey << " has " << this->m_dataStructInterfacePtr->length(this->m_data[hashedKey]) << " existing entry/entries" << std::endl;
+    KeyValPair<T, U>* kvpPtr = new KeyValPair<T, U>(key, val); // copies the key and value into new key-value pair object
+    this->m_dataStructInterfacePtr->append(this->m_data[hashedKey], kvpPtr); // use data structure interface to append onto the chain
     this->m_numEntries++;
 }
 
 /* remove entry from hash table by key */
-template <class T, class U>
-bool HashTable_SeperateChaining<T, U>::remove(const T& key)
+template <class T, class U, class V>
+bool HashTable_SeperateChaining<T, U, V>::remove(const T& key)
 {
     HashTable_SearchResult<T, U> result = this->find(key);
     if (result.m_kvpPtr == nullptr)
     {
         return false; // object not found inside hash table
     }
-    this->m_data[result.m_hashedKey].remove(result.m_sepChainInd);
+    this->m_dataStructInterfacePtr->remove(this->m_data[result.m_hashedKey], result.m_sepChainInd); // use data structure interface to remove from the chain
     std::cout << "Removal : Key " << key << " and value pair removed from dynamic array @ index " << result.m_hashedKey << std::endl;
     this->m_numEntries--;
+}
+
+/* display 
+    - note the value datatype must be defined for ostream operator
+*/
+template <class T, class U, class V>
+void HashTable_SeperateChaining<T, U, V>::display()
+{
+    std::cout << "======================" << std::endl;
+    for (unsigned int i = 0; i < this->m_arrLength; ++i)
+    {
+        std::cout << "Index (" << i << "):" << std::endl;
+        for (unsigned int j = 0; j < this->m_dataStructInterfacePtr->length(this->m_data[i]); ++j)
+        {
+            KeyValPair<T, U>* kvpPtr = this->m_dataStructInterfacePtr->get(this->m_data[i], j);
+            std::cout << "\t Key: " << kvpPtr->m_key << " -> Value: " << kvpPtr->m_val << std::endl;
+        }
+    }
+    std::cout << "======================" << std::endl;
 }
 
 /********************************************************************************************************************/
@@ -224,7 +331,7 @@ public:
     HashTable_OpenAddressing() = delete;
     HashTable_OpenAddressing(const unsigned int& length, HashFunctor<T>* hashFunctionPtr, ProbeFunctor* probeFunctionPtr, const double& maxLoadFactor);
     ~HashTable_OpenAddressing();
-    HashTable_SearchResult<T, U> find(const T& key);
+    HashTable_SearchResult<T, U> find(const T& key) const;
     void insert(const T& key, const U& val);
     bool remove(const T& key);
 };
@@ -251,7 +358,7 @@ HashTable_OpenAddressing<T, U>::~HashTable_OpenAddressing()
 
 /* returns the pointer to a key value pair given key */
 template <class T, class U>
-HashTable_SearchResult<T, U> HashTable_OpenAddressing<T, U>::find(const T& key)
+HashTable_SearchResult<T, U> HashTable_OpenAddressing<T, U>::find(const T& key) const
 {  
     unsigned int hashedKey = (*this->m_hashFunctionPtr)(key, this->m_arrLength);
     if (this->m_data[hashedKey]->m_key == key)
@@ -283,9 +390,7 @@ void HashTable_OpenAddressing<T, U>::insert(const T& key, const U& val)
 
     unsigned int hashedKey = (*this->m_hashFunctionPtr)(key, this->m_arrLength);
     std::cout << "Insertion : Key " << key << " maps onto index " << hashedKey << std::endl;
-    KeyValPair<T, U>* kvpPtr = new KeyValPair<T, U>; // copies the key and value
-    kvpPtr->m_key = key;
-    kvpPtr->m_val = val;
+    KeyValPair<T, U>* kvpPtr = new KeyValPair<T, U>(key, val); // copies the key and value
     this->m_probeFunctionPtr->reset(); // reset probe function ptr to base value
     unsigned int ind = hashedKey;
     while (this->m_data[ind] != nullptr)
--- testing/main.cpp
+++ testing/main.cpp
@@ -1,25 +1,26 @@
 #include <iostream>
 
-#include <HashTable.hpp>
+#include "../include/HashTable.hpp"
 #include <string>
 
 class MyHashFunctor : public datastructlib::HashFunctor<unsigned int>
 {
 public:
-    unsigned int operator()(const unsigned int& key, const unsigned int& arrSize)
+    unsigned int operator()(const unsigned int& key, const unsigned int& arrLength)
     {
-        return (key*key + 3) % arrSize;
+        return (key*key + 3) % arrLength;
     }
 };
-
-
 
 int main() 
 {
     MyHashFunctor hashFunc;
     datastructlib::LinearProbeFunctor probeFunc(3, 0);
     
-    datastructlib::HashTable_OpenAddressing<unsigned int, std::string> ht(10, &hashFunc, &probeFunc, 1.0);
+    //datastructlib::HashTable_DynamicArrayInterface<unsigned int, std::string> interface;
+    datastructlib::HashTable_SinglyLinkedListInterface<unsigned int, std::string> interface;
+
+    datastructlib::HashTable_SeperateChaining< unsigned int, std::string, datastructlib::SinglyLinkedList< datastructlib::KeyValPair< unsigned int, std::string >* > > ht(10, &interface, &hashFunc);
 
     ht.insert(25, "Maths");
     ht.insert(41, "Chemistry");
@@ -27,7 +28,17 @@ int main()
     ht.insert(12, "Maths");
     ht.insert(81, "Chemistry");
     ht.insert(46, "Physics");
+    ht.insert(42, "Maths");
+    ht.insert(39, "Chemistry");
+    ht.insert(92, "Physics");
+    ht.insert(4, "Maths");
+    ht.insert(15, "Chemistry");
+    ht.insert(61, "Physics");
     
     datastructlib::HashTable_SearchResult<unsigned int, std::string> result = ht.find(25);
+    std::cout << result.m_kvpPtr << std::endl;
     std::cout << result.m_kvpPtr->m_val << std::endl;
+    std::cout << std::endl;
+
+    ht.display();
 }
